a17475e1f95e0f6d2c24ca56b8daf7bf48d77e3e
Brian Whitton <me@brianwhitton.com>
2014-04-09 22:00:24 -0400

    adds drinks option

--- app/controllers/drinks_controller.rb
+++ app/controllers/drinks_controller.rb
@@ -1,2 +1,13 @@
 class DrinksController < ApplicationController
+  def index
+    if params[:id]
+      # get venue by id
+      @venue = get_a_venue(:drinks)
+    else
+      @venue = get_a_venue(:drinks)
+    end
+  end
+
+  def show
+  end
 end
